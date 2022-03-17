a7d3242645295aafde06c5897a8b10bbd0d4c0e3
acourney <acourney@ucsc.edu>
2022-03-17 15:37:10 -0700

    scaffold out components and add get request in BookmarkList

---
+++ src/components/BookmarkList.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from React;
+import BookmarkItem from "./BookmarkItem";
+import axios from "axios";
+
+export default function BookmarkList() {
+  const [bookmarks, setBookmarks] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(async () => {
+    try {
+      const res = await axios.get('http://localhost:8000/api/bookmarks')
+      setBookmarks(res);
+    }
+    catch (error) {console.log(error)}
+  }, [])
+
+  return (<ul>{bookmarks.map((bookmark) => (
+    <BookmarkItem key={bookmark.id} bookmark={bookmark} />
+  ))}
+  <li>test from bookmark list</li></ul>);
+
+}
